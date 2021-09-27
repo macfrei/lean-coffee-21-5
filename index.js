864c35f5fd5bb0ce434b2e8fb0e8165776c4a254
Marie-Carleen Frei <lene@7freiheiten.de>
2021-09-27 15:40:22 +0200

    Rebuild http server with express

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html; charset=utf-8')
-  res.end('<h1>Hello, World! What is up?</h1>')
+app.get('/api/cards', (request, response) => {
+  response.set('Content-type', 'text/html; charset=utf-8')
+  response.send('<h1>Hello, World! Was läuft?</h1>')
 })
 
-server.listen(port, () => {
-  console.log(`Server running at port ${port}`)
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
 })
