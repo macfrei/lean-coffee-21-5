e5c3f9e711b075294c56f483a68370402453dce6
Marie-Carleen Frei <lene@7freiheiten.de>
2021-09-27 15:12:38 +0200

    Build simple http server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+const http = require('http')
+
+const port = 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  res.end('<h1>Hello, World! What is up?</h1>')
+})
+
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`)
+})
