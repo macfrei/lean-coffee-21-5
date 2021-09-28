6111726e9fe3cda1e69a683b5fd91e438ce0201d
Marie-Carleen Frei <lene@7freiheiten.de>
2021-09-28 15:02:12 +0200

    Implement post request handler

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { nanoid } = require('nanoid')
 
 const router = express.Router()
 
@@ -33,16 +34,27 @@ router.get('/:id', (request, response) => {
     const error = { message: 'Could not find object with that id.' }
     response.status(404).json(error)
   }
+
+  // if(!card) {
+  //   const error = { message: 'Could not find object with that id.' }
+  //   return response.status(404).json(error)
+  // }
+
+  // return response.status(200).json(card)
 })
 
 router.post('/', (request, response) => {
-  console.log(request.body)
+  const { text, author } = request.body // { text: "What is node?", author: "Max M." }
 
-  const requestObject = request.body
-  response.send(requestObject.text)
+  if (text === '' || author === '') {
+    const error = { message: 'Information missing.' }
+    return response.status(400).json(error) // Bad Request
+  }
 
-  //   const {text} = request.body
-  //   response.send(text)
+  const newCard = { text, author, id: nanoid() } // { text: text, author: author, id: nanoid() }
+  cards = [...cards, newCard]
+  // cards.push(newCard)
+  response.status(200).json(newCard)
 })
 
 router.put('/:id', (request, response) => {
