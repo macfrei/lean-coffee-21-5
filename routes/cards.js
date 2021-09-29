a53117e6dd298db1b7a831dbb1ad18d15e34fae7
Marie-Carleen Frei <lene@7freiheiten.de>
2021-09-29 09:50:59 +0200

    Clean up

--- routes/cards.js
+++ routes/cards.js
@@ -22,38 +22,28 @@ router.get('/', (request, response) => {
 
 router.get('/:id', (request, response) => {
   const { id } = request.params
-  // const params = request.params // { id: '1234abc' }
-  // const id = params.id // '1234abc'
 
   const card = cards.find(card => card.id === id)
-  // card === undefined: falsy
-  // card === {...}: truthy
+
   if (card) {
     response.status(200).json(card)
   } else {
     const error = { message: 'Could not find object with that id.' }
     response.status(404).json(error)
   }
-
-  // if(!card) {
-  //   const error = { message: 'Could not find object with that id.' }
-  //   return response.status(404).json(error)
-  // }
-
-  // return response.status(200).json(card)
 })
 
 router.post('/', (request, response) => {
-  const { text, author } = request.body // { text: "What is node?", author: "Max M." }
+  const { text, author } = request.body
 
   if (text === '' || author === '') {
     const error = { message: 'Information missing.' }
-    return response.status(400).json(error) // Bad Request
+    return response.status(400).json(error)
   }
 
-  const newCard = { text, author, id: nanoid() } // { text: text, author: author, id: nanoid() }
+  const newCard = { text, author, id: nanoid() }
   cards = [...cards, newCard]
-  // cards.push(newCard)
+
   response.status(200).json(newCard)
 })
 
@@ -91,9 +81,8 @@ router.patch('/:id', (request, response) => {
   const { text, author } = request.body
 
   if (!text && !author) {
-    // wenn text UND auhtor beide leer sind, dann schmeiß einen Error
     const error = { message: 'Information missing.' }
-    return response.status(400).json(error) // Bad Request
+    return response.status(400).json(error)
   }
 
   const card = cards.find(card => card.id === id)
@@ -109,14 +98,9 @@ router.patch('/:id', (request, response) => {
     id: card.id,
   }
 
-  const index = cards.findIndex(card => card.id === id) // gives index of card
-
+  const index = cards.findIndex(card => card.id === id)
   cards = [...cards.slice(0, index), newCard, ...cards.slice(index + 1)]
 
-  // const firstHalfOfArray = cards.slice(0, index)
-  // const lastHalfofArray = cards.slice(index + 1)
-
-  // cards = [...firstHalfOfArray, newCard, ...lastHalfofArray]
   response.status(200).json(newCard)
 })
 
