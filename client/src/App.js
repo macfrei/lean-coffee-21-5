8997726faf25e747c767881a6350b68db7acac2f
Marie-Carleen Frei <lene@7freiheiten.de>
2021-10-04 15:41:22 +0200

    Add fetch with POST reques

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import styled from 'styled-components/macro'
+import Form from './components/Form'
 import getCards from './services/getCards'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
 
   return (
     <Main>
+      <Form onCreateCard={createCard} />
       {cards.map(card => (
         <section key={card._id}>
           <p>{card.text}</p>
@@ -23,10 +25,21 @@ function App() {
     </Main>
   )
 
-  function createCard() {
+  function createCard(card) {
+    console.log(card)
     // postCard.js mit function postCard(card)
     // POST '/api/cards'
     // .then(card => setCards([...cards, card]))
+    fetch('/api/cards', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify(card),
+    })
+      .then(res => res.json())
+      .then(data => setCards([...cards, data])) // {text: "What is HTML?", author: "Anonymous", _id: "231243f3jghv"}
+      .catch(error => console.error(error))
   }
 }
 
@@ -34,12 +47,10 @@ const Main = styled.main`
   display: grid;
   grid-gap: 20px;
   height: 100vh;
-  background-color: hotpink;
-  color: white;
   padding: 30px;
 
   section {
-    border: 2px solid white;
+    border: 2px solid black;
   }
 `
 
